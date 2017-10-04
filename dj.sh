91a30f7f1aa327eba76108a87b5bbfeba8ef232a
E524 <tristand.pro@gmail.com>
2017-10-04 21:01:37 +0200

    Create dj.sh

---
+++ dj.sh
@@ -0,0 +1,36 @@
+PROJECT="streos/"
+APPLICATIONS=("baseapp" "cart" "orders" "payment" "shop")
+RETURNCD="../.."
+
+
+echo "What do you do?"
+select i in run makemigrations migrate showmigrations \test; do
+        if [ "$i" = "run" ]; then
+                cd $PROJECT
+                python3 manage.py runserver
+                exit
+                break
+        elif [ "$i" = "makemigrations" ]; then
+                cd $PROJECT
+                python3 manage.py makemigrations
+                exit
+                break
+        elif [ "$i" = "migrate" ]; then
+                cd $PROJECT
+                python3 manage.py migrate
+                exit
+                break
+        elif [ "$i" = "showmigrations" ]; then
+                cd $PROJECT
+                python3 manage.py showmigrations
+                exit
+                break
+        elif [ "$i" = "test" ]; then
+                cd $PROJECT
+                python3 manage.py test
+                exit
+                break
+        else
+                echo "Ceci n'existe pas"
+        fi
+done
