f3d4f163353f1064069f5467ff63d83f752cda89
Nelestya <tristand.pro@gmail.com>
2017-10-21 23:46:08 +0200

    add deletemigration and create superuser and a dev loaddata

--- dj.sh
+++ dj.sh
@@ -1,10 +1,10 @@
 PROJECT="projectname/"
 APPLICATIONS=("baseapp" "cart" "orders" "payment" "shop")
 RETURNCD="../.."
-
+# DATAJSON=(" ") for a specific file json
 
 echo "What do you do?"
-select i in run makemigrations migrate showmigrations \test; do
+select i in run makemigrations migrate showmigrations \test createsuperuser deletemigrations; do
         if [ "$i" = "run" ]; then
                 cd $PROJECT
                 python3 manage.py runserver
@@ -30,7 +30,31 @@ select i in run makemigrations migrate showmigrations \test; do
                 python3 manage.py test
                 exit
                 break
+        elif [ "$i" = "createsuperuser" ]; then
+                cd $PROJECT
+                python3 manage.py createsuperuser
+                exit
+                break
+        elif [ "$i" = "loaddata" ]; then
+                cd $PROJECT
+                #python3 manage.py loaddata $APPLICATIONS/fixtures/*
+                exit
+                break
+        elif [ "$i" = "deletemigrations" ]; then
+                cd $PROJECT
+                find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
+                find . -path "*/migrations/*.pyc"  -delete
+                # if you delete the database
+                find . -name "db.sqlite3" -delete
+                exit
+                break
+
         else
+<<<<<<< HEAD
+
+                echo "Ceci n'existe pas"
+=======
                 echo "It's not exist"
+>>>>>>> a3906165e97218d53c19c28b32c835b998bcaa0b
         fi
 done
